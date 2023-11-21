7e3c12130bbaf52b5c11cd31f68efc8fda7090c2
sdmancik <shawn.mancik@selu.edu>
2023-11-21 20:03:43 +0000

    Finished Funfunction.

--- mancik.pkg/R/funfunction.R
+++ mancik.pkg/R/funfunction.R
@@ -1,5 +1,21 @@
+meme <- function (img) { 
 library(jpeg)
-
 img <- readJPEG("data/1n7w77.jpg")
 plot(1, type = "n", xlim = c(0, 10), ylim = c(0, 10))
 rasterImage(img, 0, 0, 10, 10)
+
+
+}
+
+meme()
+
+Fun_function <- function(crabs, column_name) {
+  if (!column_name %in% names(crabs)) {
+    stop(meme())
+  }
+  
+  crabs_ordered <- crabs %>%
+    arrange(across(({{column_name}})))
+  
+  return(crabs_ordered)
+}
